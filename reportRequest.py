3c018d3b355f7ae8f9dd1346b8dbfb9ba9284655
Ariel Nathan <nathanariel17@gmail.com>
2021-03-18 18:35:26 -0400

    Initial Commit

---
+++ reportRequest.py
@@ -0,0 +1,80 @@
+import csv
+import time
+import os
+from credentials import *
+from reports import *
+from datetime import date
+from mws import Products, Reports, Marketplaces, Inventory
+
+today = date.today()
+
+account_id = MWS_ACCOUNT_ID
+access_key = MWS_ACCESS_KEY
+secret_key = MWS_SECRET_KEY
+
+reports_api = Reports(
+    access_key = access_key,
+    secret_key = secret_key,
+    account_id = account_id
+)
+
+reports_api._use_feature_mwsresponse = True
+
+def requestReport(reportName):
+    print("Requesting Report")
+    reportRequest = reports_api.request_report(reportName)
+    reportRequest = reportRequest.parsed
+    reportRequestId = reportRequest.ReportRequestInfo.ReportRequestId
+    print("Report Requested with Request ID: " + reportRequestId)
+    getReportStatus(reportRequestId)
+
+def getReportStatus(reportRequestId):
+    print("Getting Report Status")
+    reportList = reports_api.get_report_request_list(reportRequestId)
+    reportList = reportList.parsed
+    reportStatus = reportList.ReportRequestInfo.ReportProcessingStatus
+    print(reportStatus)
+    checkStatus(reportStatus, reportRequestId)
+
+def checkStatus(reportStatus, reportRequestId):
+    if reportStatus == "_DONE_":
+        print("Report Status: Done")
+        reportData = reports_api.get_report_list(reportRequestId)
+        reportData = reportData.parsed
+        reportId = reportData.ReportInfo.ReportId
+        reportType = reportData.ReportInfo.ReportType
+        print("Report ID: " + reportId)
+        report = reports_api.get_report(reportId)
+        report = report.parsed
+        print("Report Retreived")
+        writeFile(report, reportType)
+    elif reportStatus == "_SUBMITTED_":
+        print("Report Status: Submitted")
+        print("Waiting a minute before next check")
+        time.sleep(60)
+        getReportStatus(reportRequestId)
+    elif reportStatus == "_CANCELLED_":
+        print("Report Status: Cancelled")
+    elif reportStatus == "_DONE_NO_DATA_":
+        print("Report Status: Done with no Data")
+
+def writeFile(report, reportType):
+    print("Writing File")
+    txtName = "TemporaryFile.txt"
+    csvName = reportType + str(today) + ".csv"
+
+    with open(txtName, mode='w') as f:
+        f.write(report)
+    
+    with open(txtName, "r") as in_text:
+        in_reader = csv.reader(in_text, delimiter = '\t')
+        with open(csvName, "w") as out_csv:
+            out_writer = csv.writer(out_csv)
+            for row in in_reader:
+                out_writer.writerow(row)
+    
+    os.remove(txtName)
+
+    print("Done")
+
+requestReport(FBA_INVENTORY_AFN)
